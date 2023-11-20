c143e742a5c21118152449560d554fd1ac97aec4
Родион Шукало <ban.zhmyh@yandex.ru>
2023-11-20 23:37:51 +0300

    code according to PEP8

---
+++ backend/import_data.py
@@ -0,0 +1,18 @@
+import os
+import django
+import pandas as pd
+from food.models import Ingredient
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+django.setup()
+
+def import_data(csv_path):
+    df = pd.read_csv(csv_path)
+
+    for index, row in df.iterrows():
+        Ingredient.objects.create(
+            name=row['name'], measurement_unit=row['measurement_unit']
+        )
+
+if __name__ == "__main__":
+    import_data('C:/Dev/foodgram-project-react/data/ingredients.csv')
