113683af4115e2925f6fd79ffd066eb430a6edf5
Родион Шукало <ban.zhmyh@yandex.ru>
2023-11-24 18:45:54 +0300

    code according to PEP8

--- backend/import_data.py
+++ backend/import_data.py
@@ -1,4 +1,3 @@
-
 import os
 import django
 import pandas as pd
@@ -8,19 +7,21 @@ django.setup()
 
 from food.models import Ingredient
 
-def import_data(csv_path):
 
+def import_data(csv_path):
     df = pd.read_csv(csv_path)
 
-
     for index, row in df.iterrows():
-        Ingredient.objects.create(name=row['name'], measurement_unit=row['measurement_unit'])
+        Ingredient.objects.create(
+            name=row['name'], measurement_unit=row['measurement_unit']
+        )
+
 
 if __name__ == "__main__":
     data_path = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)),
-            'data/',
-            'ingredients.csv',
-        )
+        os.path.dirname(os.path.abspath(__file__)),
+        'data/',
+        'ingredients.csv',
+    )
 
     import_data(data_path)
